323ff81f5999af98be2dae6eb61f82d387a60475
Scarlett <scarlettkkszekai@gmail.com>
2019-02-20 22:30:58 +0800

    learn more about Redux work flow, action dispatch, change state and subscribe

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,14 +7,24 @@ class TodoList extends Component {
     constructor(props){
       super(props)
       this.state = store.getState();
-      console.log(store.getState())
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleStoreChange = this.handleStoreChange.bind(this);
+      this.handleBtnClick = this.handleBtnClick.bind(this);
+      store.subscribe(this.handleStoreChange)
+      //这里需要订阅store的改变，这样reducer的state改变之后才会更新到组件
+      //store一旦改变就会调用this.handleStoreChange方法
     }
     
     render() {
       return (
         <div>
-          <Input value={this.state.inputValue} placeholder="Todo Info" style={{width:'300px', margin:'10px'}}/>
-          <Button type="primary">Submit</Button>
+          <Input 
+            value={this.state.inputValue} 
+            placeholder="Todo Info" 
+            style={{width:'300px', margin:'10px'}}
+            onChange={this.handleInputChange}
+          />
+          <Button type="primary" onClick={this.handleBtnClick}>Submit</Button>
           <List
             style={{width:'500px', margin:'10px'}}
             bordered
@@ -23,6 +33,25 @@ class TodoList extends Component {
           />
         </div>
       )
+    }
+
+    handleInputChange(e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }// 创建一个 action（借书的那句话）
+      store.dispatch(action) // 将要借书的这句话传递给store，store会转发给reducer
+    }
+
+    handleStoreChange(){
+      this.setState(store.getState())//更新当前组件的state
+    }
+
+    handleBtnClick() {
+      const action = {
+        type: 'add_todo_item'
+      }// 创建一个 action（借书的那句话）
+      store.dispatch(action) // 将要借书的这句话传递给store，store会转发给reducer
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,21 @@ const defaultState = {
 	list: [1, 2]
 }
 
+// reducer可以接收state，但绝不能改变state
 export default (state = defaultState, action) => {
+	// state：老的state值  action：事件类别（必须有），以及传递过来的state的某个属性的新值(可为空)
+	if(action.type === 'change_input_value') {
+		const newState = JSON.parse(JSON.stringify(state)); //深拷贝的骚操作
+		newState.inputValue = action.value;//复制一份当前state之后，将state里面的值替换成最新值
+		return newState;// 最后返回
+	}
+	if(action.type === 'add_todo_item') {
+		const newState = JSON.parse(JSON.stringify(state)); //深拷贝的骚操作
+		if(newState.inputValue !== '')
+			newState.list.push(newState.inputValue);
+		newState.inputValue = '';
+		return newState;// 最后返回
+	}
 	return state;
 	// state：整个store仓库里面存储的数据
 }
