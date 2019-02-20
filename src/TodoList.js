1f10199f8ff6d858443871ba7dbf1d93c7c15740
Scarlett <scarlettkkszekai@gmail.com>
2019-02-20 21:34:44 +0800

    learn how to create store & reducer

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'; 
 import { Input, Button, List } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import store from './store/index'
 
 class TodoList extends Component {
+    constructor(props){
+      super(props)
+      this.state = store.getState();
+      console.log(store.getState())
+    }
     
     render() {
       return (
         <div>
-          <Input placeholder="Todo Info" style={{width:'300px', margin:'10px'}}/>
+          <Input value={this.state.inputValue} placeholder="Todo Info" style={{width:'300px', margin:'10px'}}/>
           <Button type="primary">Submit</Button>
           <List
             style={{width:'500px', margin:'10px'}}
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => (<List.Item>{item}</List.Item>)}
           />
         </div>
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+	inputValue: '123',
+	list: [1, 2]
+}
+
+export default (state = defaultState, action) => {
+	return state;
+	// state：整个store仓库里面存储的数据
+}
